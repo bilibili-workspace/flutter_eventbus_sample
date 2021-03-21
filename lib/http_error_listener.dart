be47b414cb392261a187f76c1477def13d7cf718
FishInWater-1999 <814484626@qq.com>
2021-03-21 20:07:40 +0800

    eventbus  点击 toast
    eventbus  点击 toast

---
+++ lib/http_error_listener.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'event_bus.dart';
+import 'home_page.dart';
+import 'http_error_event.dart';
+
+mixin HttpErrorListener on State<MyHomePage> {
+  StreamSubscription stream;
+
+  ///这里为什么用 _context 你理解吗？
+  ///因为此时 State 的 context 是 FlutterReduxApp 而不是 MaterialApp
+  ///所以如果直接用 context 是会获取不到 MaterialApp 的 Localizations 哦。
+  BuildContext _context;
+
+  @override
+  void initState() {
+    super.initState();
+
+    ///Stream演示event bus
+    stream = eventBus.on<HttpErrorEvent>().listen((event) {
+      errorHandleFunction(event.code, event.message);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (stream != null) {
+      stream.cancel();
+      stream = null;
+    }
+  }
+
+  ///网络错误提醒
+  errorHandleFunction(int code, message) {
+    switch (code) {
+      case 404:
+        showToast("404: ${message}");
+        break;
+      default:
+        showToast("unknown: ${message}");
+        break;
+    }
+  }
+
+  showToast(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        gravity: ToastGravity.CENTER,
+        toastLength: Toast.LENGTH_LONG);
+  }
+}
